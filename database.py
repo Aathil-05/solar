126c56bab1d36188a509fdd679b235c70fd55d99
Mohamed Aathil <aathilmohamed998@gmail.com>
2026-01-11 23:46:17 +0530

    Added the Url

--- database.py
+++ database.py
@@ -11,7 +11,12 @@ DATABASE_URL = os.getenv(
     "mysql+pymysql://root:aathil12@localhost:3306/solardata"
 )
 
-engine = create_engine()
+# DATABASE_URL = os.getenv("DATABASE_URL", 
+#     "mysql+pymysql://solar_highwayhow:7b0e3ba3b8fcff9bfefcab1e11e659e0f7465375@fu5ryk.h.filess.io:3307/solar_highwayhow")
+
+engine = create_engine(
+    DATABASE_URL
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
